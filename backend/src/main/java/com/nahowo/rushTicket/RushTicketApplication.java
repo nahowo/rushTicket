60521bb18da565fdad973d30e214d106851c399a
nahowo <nahowo@naver.com>
2025-08-19 20:59:41 +0900

    initial commit

---
+++ backend/src/main/java/com/nahowo/rushTicket/RushTicketApplication.java
@@ -0,0 +1,13 @@
+package com.nahowo.rushTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RushTicketApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/nahowo/rushTicket/RushTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahowo.rushTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

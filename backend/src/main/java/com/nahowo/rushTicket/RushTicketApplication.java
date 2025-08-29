06c97253a1ff7dfad4b023692974414279b8b38b
nahowo <nahowo@naver.com>
2025-08-29 11:53:43 +0900

    RT-10 set: 환경변수 위치 변경

--- backend/src/main/java/com/nahowo/rushTicket/RushTicketApplication.java
+++ backend/src/main/java/com/nahowo/rushTicket/RushTicketApplication.java
@@ -3,12 +3,10 @@ package com.nahowo.rushTicket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EntityScan(basePackages = {"com.nahowo.rushTicket.domain"})
-@PropertySource("classpath:env.properties")
 @SpringBootApplication
 public class RushTicketApplication {
 

2e89ac972cc05801ddc0abe266741a203e299d4c
nahowo <nahowo@naver.com>
2025-09-01 17:31:43 +0900

    RT-11 feat: createUser 비밀번호 인코딩

--- backend/src/main/java/com/nahowo/rushTicket/service/UserService.java
+++ backend/src/main/java/com/nahowo/rushTicket/service/UserService.java
@@ -7,19 +7,20 @@ import com.nahowo.rushTicket.dto.response.UserResponse;
 import com.nahowo.rushTicket.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserResponse createUser(UserCreateRequest reqeust) {
         String name = reqeust.name();
         String email = reqeust.email();
-        String password = reqeust.password();
+        String password = passwordEncoder.encode(reqeust.password());
         isEmailDuplicated(email);
         User createdUser = userRepository.save(new User(name, email, password));
         return new UserResponse(createdUser);

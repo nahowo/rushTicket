11961e12a370f4f355789bb1dc71073e03b727b5
nahowo <nahowo@naver.com>
2025-09-01 12:50:10 +0900

    RT-20 feat: 이메일 중복 예외처리

--- backend/src/main/java/com/nahowo/rushTicket/service/UserService.java
+++ backend/src/main/java/com/nahowo/rushTicket/service/UserService.java
@@ -6,10 +6,12 @@ import com.nahowo.rushTicket.dto.response.UserResponse;
 import com.nahowo.rushTicket.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
@@ -18,7 +20,19 @@ public class UserService {
         String name = reqeust.getName();
         String email = reqeust.getEmail();
         String password = reqeust.getPassword();
+        try {
+            isEmailDuplicated(email);
+        } catch (Exception e) {
+            log.error("이메일 중복: " + email);
+            return null;
+        }
         User createdUser = userRepository.save(new User(name, email, password));
         return new UserResponse(createdUser);
+    }
+
+    private void isEmailDuplicated(String email) throws Exception{
+        if (userRepository.existsByEmail(email)) {
+            throw new Exception();
+        }
     }
 }

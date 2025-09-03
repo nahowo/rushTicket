35421872b1bc3057a9baeee0aa642fe4d3f78aba
nahowo <nahowo@naver.com>
2025-09-03 11:11:58 +0900

    RT-24 feat: VenueAvailability를 VenueReservation으로 변경, 필드 변경

---
+++ backend/src/main/java/com/nahowo/rushTicket/domain/VenueReservation.java
@@ -0,0 +1,22 @@
+package com.nahowo.rushTicket.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "venue_reservation")
+public class VenueReservation extends BaseEntity{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDate eventDate;
+}

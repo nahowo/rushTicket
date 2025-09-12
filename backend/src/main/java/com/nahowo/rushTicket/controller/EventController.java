e6ccd49b06b4ed7b5149305d13f21452acd601b5
nahowo <nahowo@naver.com>
2025-09-12 11:49:59 +0900

    RT-25 feat: 이벤트 등록 컨트롤러 작성

---
+++ backend/src/main/java/com/nahowo/rushTicket/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.nahowo.rushTicket.controller;
+
+import com.nahowo.rushTicket.config.result.ResultCode;
+import com.nahowo.rushTicket.config.result.ResultResponse;
+import com.nahowo.rushTicket.dto.request.EventCreateRequest;
+import com.nahowo.rushTicket.dto.response.EventResponse;
+import com.nahowo.rushTicket.service.EventService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Events")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/events")
+public class EventController {
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<ResultResponse> createEvent(@RequestBody @Valid EventCreateRequest request) {
+        EventResponse event = eventService.createEvent(request);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.EVENT_CREATE, event));
+    }
+}

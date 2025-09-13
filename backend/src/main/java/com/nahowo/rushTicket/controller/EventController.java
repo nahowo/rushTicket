c484cf1d229fd1be856015df30e0058ed5582323
nahowo <nahowo@naver.com>
2025-09-13 17:01:08 +0900

    RT-25 feat: 이벤트 수정 컨트롤러 작성

--- backend/src/main/java/com/nahowo/rushTicket/controller/EventController.java
+++ backend/src/main/java/com/nahowo/rushTicket/controller/EventController.java
@@ -3,12 +3,14 @@ package com.nahowo.rushTicket.controller;
 import com.nahowo.rushTicket.config.result.ResultCode;
 import com.nahowo.rushTicket.config.result.ResultResponse;
 import com.nahowo.rushTicket.dto.request.EventCreateRequest;
+import com.nahowo.rushTicket.dto.request.EventUpdateRequest;
 import com.nahowo.rushTicket.dto.response.EventResponse;
 import com.nahowo.rushTicket.service.EventService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/events")
 public class EventController {
+
     private final EventService eventService;
 
     @PostMapping
-    public ResponseEntity<ResultResponse> createEvent(@RequestBody @Valid EventCreateRequest request) {
+    public ResponseEntity<ResultResponse> createEvent(
+        @RequestBody @Valid EventCreateRequest request) {
         EventResponse event = eventService.createEvent(request);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.EVENT_CREATE, event));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ResultResponse> updateEvent(
+        @RequestBody @Valid EventUpdateRequest request) {
+        EventResponse event = eventService.updateEvent(request);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.EVENT_UPDATE, event));
     }
 }

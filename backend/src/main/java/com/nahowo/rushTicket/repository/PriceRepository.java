f0a9cc6c199834d1860ad197cb3be0ac1f689b03
nahowo <nahowo@naver.com>
2025-09-14 12:42:18 +0900

    RT-25 feat: 레포지토리 메서드 추가

--- backend/src/main/java/com/nahowo/rushTicket/repository/PriceRepository.java
+++ backend/src/main/java/com/nahowo/rushTicket/repository/PriceRepository.java
@@ -1,8 +1,12 @@
 package com.nahowo.rushTicket.repository;
 
+import com.nahowo.rushTicket.domain.EventDateTime;
 import com.nahowo.rushTicket.domain.Price;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PriceRepository extends JpaRepository<Price, Long> {
+
+    List<Price> findAllByEventDateTime(EventDateTime eventDateTime);
 
 }

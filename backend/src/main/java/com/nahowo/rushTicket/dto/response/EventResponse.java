bdbd6a10040f968d7bf77c59e7a6654c0fad6a57
nahowo <nahowo@naver.com>
2025-09-14 11:49:44 +0900

    RT-25 feat: 이벤트 반환 DTO 수정

--- backend/src/main/java/com/nahowo/rushTicket/dto/response/EventResponse.java
+++ backend/src/main/java/com/nahowo/rushTicket/dto/response/EventResponse.java
@@ -1,16 +1,22 @@
 package com.nahowo.rushTicket.dto.response;
 
 import com.nahowo.rushTicket.domain.Event;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 import lombok.Getter;
 
 @Getter
 public class EventResponse {
-    Map<Long, Map<Long, BigDecimal>> prices;
+    private Long id;
+    private LocalDateTime booking_start_time;
+    private LocalDateTime booking_end_time;
+    private String description;
+    private String name;
 
-    public EventResponse(Map<Long, Map<Long, BigDecimal>> prices) {
-        this.prices = prices;
+    public EventResponse(Event event) {
+        this.id = event.getId();
+        this.booking_start_time = event.getBookingStartTime();
+        this.booking_end_time = event.getBookingEndTime();
+        this.description = event.getDescription();
+        this.name = event.getName();
     }
 }
